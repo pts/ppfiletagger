6be5604b23ab7bfcb131b1feb7dded9eaba6689d
Peter Szabo <pts@fazekas.hu>
2008-11-04 23:27:22 +0000

    added _mmfs_unify_tags

--- ppfiletagger_shell_functions.sh
+++ ppfiletagger_shell_functions.sh
@@ -43,6 +43,114 @@ print "modified tags of $C file@{[$C==1?q():q(s)]}: $tags\n"
 END
 }
 
+#** Makes both files have the union of the tags.
+#** Imp: also unify the descriptions.
+#** SUXX: needed 2 runs: modified 32, then 4, then 0 files (maybe because of
+#**   equivalence classes)
+#** @example _mmfs_unify_tags file1 file2
+#** @example echo "... 'file1' ... 'file2' ..." ... | _mmfs_unify_tags --stdin
+function _mmfs_unify_tags() {
+	perl -we '
+use Cwd;
+$ENV{LC_MESSAGES}=$ENV{LANGUAGE}="C"; # Make $! English
+use integer; use strict;  $|=1;
+require "syscall.ph";
+my $SYS_setxattr=&SYS_setxattr;
+my $SYS_getxattr=&SYS_getxattr;
+my $mmdir="$ENV{HOME}/mmfs/root/";
+my $C=0;  my $EC=0;
+$0="_mmfs_unify_tags";
+die "Usage: $0 <file1> <file2>
+     or echo \"... \x27file1\x27 ... \x27file2\x27 ...\" ... | $0 --stdin\n" if
+     @ARGV!=2 and @ARGV!=1;
+print "unifying tags\n";
+
+#** @return :String, may be empty
+sub get_tags($) {
+  my $fn=Cwd::abs_path($_[0]);
+  substr($fn,0,1)=$mmdir if substr($fn,0,length$mmdir)ne$mmdir;
+  #print "  $fn\n";
+  # vvv Imp: move, not setfattr
+  my $key="user.mmfs.tags"; # Dat: must be in $var
+  my $tags="\0"x65535;
+  my $got=syscall($SYS_getxattr, $fn, $key, $tags,
+    length($tags), 0);
+  if (!defined $got or $got<0) {
+    print "    error: $fn: $!\n"; $EC++;
+    return "";
+  } else {
+    $tags=~s@\0.*@@s;
+    return $tags;
+  }
+}
+
+sub add_tags($$) {
+  my($fn0,$tags)=@_;
+  my $fn=Cwd::abs_path($fn0);
+  substr($fn,0,0)=$mmdir if substr($fn,0,length$mmdir)ne$mmdir;
+  #print "  $fn\n";
+  my $key="user.mmfs.tags.modify"; # Dat: must be in $var
+  my $got=syscall($SYS_setxattr, $fn, $key, $tags,
+    length($tags), 0);
+  if (!defined $got or $got<0) {
+    if ("$!" eq "Cannot assign requested address") {
+      print "\007bad tags ($tags)\n"; $EC++;
+    } else { print "add-error: $fn: $!\n"; $EC++ }
+  } else { $C++ }
+}
+
+
+sub unify_tags($$) {
+  my($fn0,$fn1)=@_;
+  my $tags0=get_tags($fn0);
+  my $tags1=get_tags($fn1);
+  if ($tags0 eq $tags1) {
+    if ($tags0 eq "") {
+      print "neither: ($fn0) ($fn1)\n";
+      return -1
+    }
+    print "both ($tags0): ($fn0) ($fn1)\n";
+    return -2
+  }
+  #print "$tags0; $tags1\n";
+  add_tags($fn0, $tags1) if $tags1 ne "";
+  add_tags($fn1, $tags0) if $tags0 ne "";
+  
+  my $tags0b=get_tags($fn0);
+  my $tags1b=get_tags($fn1);
+  if ($tags0b eq $tags1b) {
+    print "unified ($tags0b): ($fn0) ($fn1)\n";
+  } else {
+    print "\007failed to unify: ($fn0):($tags0b), ($fn1):($tags1b)\n";
+    $EC++;
+    return -3;
+  }
+  return 0;
+}
+
+if (@ARGV==2) {
+  unify_tags($ARGV[0], $ARGV[1]);
+} else {
+  die "error: supply filename pairs in STDIN (not a TTY)\n" if -t STDIN;
+  while (<STDIN>) {
+    next if !/\S/ or /^\s*#/;
+    my @L;
+    while (/\x27((?:[^\x27]+|\x27\\\x27\x27)*)\x27/g) {
+      push @L, $1;
+      $L[-1]=~s@\x27\\\x27\x27@\x27@g;
+    }
+    if (@L!=2) { chomp; print "not two: $_\n"; $EC++; next }
+    #print "($L[0]) ($L[1])\n";
+    unify_tags($L[0], $L[1]);
+  }
+}
+
+print "\007error with $EC file@{[$EC==1?q():q(s)]}\n" if $EC;
+print "modified tags of $C file@{[$C==1?q():q(s)]}\n";
+exit 1 if $EC;
+' -- "$@"
+}
+
 #** @example _mmfs_show file1 file2 ...
 function _mmfs_show() {
 	# Midnight Commander menu for movemetafs
