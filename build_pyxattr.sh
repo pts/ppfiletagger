6a6faea1b602b48e96e71fe9976f458e8aebdf39
Peter Szabo <pts@fazekas.hu>
2021-01-22 13:27:08 +0100

    updated source URL of pyxattr; replaced setuptools with distutils in pyxattr setup.py; made linking of -lattr static if possible (/lib/libattr.a)

--- build_pyxattr.sh
+++ build_pyxattr.sh
@@ -19,7 +19,8 @@ mkdir -p "$TMPDIR"
 test -d "$TMPDIR"
 
 if test \! -f "$TMPDIR/pyxattr.tar.gz" || test "$1" == --download; then
-  if wget -O "$TMPDIR/pyxattr.tar.gz" http://kent.dl.sourceforge.net/sourceforge/pyxattr/pyxattr-0.4.0.tar.gz; then
+  # Old URL: http://kent.dl.sourceforge.net/sourceforge/pyxattr/pyxattr-0.4.0.tar.gz
+  if wget -O "$TMPDIR/pyxattr.tar.gz" http://github.com/pts/ppfiletagger/releases/download/python2.4src/pyxattr-0.4.0.tar.gz; then
     :
   else
     rm -f "$TMPDIR/pyxattr.tar.gz"
@@ -33,6 +34,10 @@ mkdir -p "$TMPDIR/pyxattr.extract"
 mv "$TMPDIR/pyxattr.extract/pyxattr-"* "$TMPDIR/pyxattr"
 test -f "$TMPDIR/pyxattr/xattr.c"
 
+# Use distutils instead of setuptools, in case setuptools is not available.
+perl -pi -e 's@^#*@#@ if m@import@ and m@setuptools@; s@^#*@@ if m@import@ and m@distutils@' "$TMPDIR/pyxattr/setup.py"
+# Link -lattr statically.
+test -f /lib/libattr.a && perl -pi -e 's@libraries=\[[^\]]*\]@extra_objects=["/lib/libattr.a"]@' "$TMPDIR/pyxattr/setup.py"
 (cd "$TMPDIR/pyxattr" && python2.4 setup.py build)
 test "$?" = 0  # Exit if subshell failed.
 
@@ -41,6 +46,6 @@ cp "$TMPDIR/pyxattr/build"/lib.*/xattr.so "$TARGETDIR/xattr.so"
 strip "$TARGETDIR/xattr.so"
 ## This also creates $TARGETDIR/pyxattr2/*.pyc
 # no main function: PYTHONPATH="$TARGETDIR:$PYTHONPATH" python2.4 "$TMPDIR/pyxattr/test/test_xattr.py"
-PYTHONPATH="$TARGETDIR:$PYTHONPATH" python2.4 -c 'import xattr; print xattr.listxattr; print xattr.setxattr'
+PYTHONPATH="$TARGETDIR:$PYTHONPATH" python2.4 -c 'import xattr; print xattr.listxattr; print xattr.setxattr; print xattr.listxattr("/bin/sh")'
 
 : All OK, see "$TARGETDIR/xattr.so"
